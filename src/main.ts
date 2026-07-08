f8cfd7a12020d441d2e2d576a7a1d46c53f45df1
mrstruijk <mrstruijk@gmail.com>
2026-07-08 20:53:19 +0200

    eslint and audit

--- src/main.ts
+++ src/main.ts
@@ -81,9 +81,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000),
-		);
+		
 	}
 
 	onunload() {}
